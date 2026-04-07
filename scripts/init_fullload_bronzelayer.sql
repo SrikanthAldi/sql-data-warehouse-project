f4d58bdad9e6e71fe186da31f6831757df6f1d6f
Srikanth Aldi <130350877+SrikanthAldi@users.noreply.github.com>
2026-04-07 11:11:12 -0500

    Update init_fullload_bronzelayer.sql

--- scripts/init_fullload_bronzelayer.sql
+++ scripts/init_fullload_bronzelayer.sql
@@ -4,7 +4,11 @@ BRONZE FULL LOAD (DBeaver Import Controlled)
 ============================================================
 */
 
-SELECT COUNT(*) FROM bronze.crm_cust_info;
+
+CREATE OR REPLACE PROCEDURE bronze.sp_full_load_reset()
+LANGUAGE plpgsql
+AS $$
+BEGIN
 
 TRUNCATE TABLE bronze.crm_cust_info;
 TRUNCATE TABLE bronze.crm_prd_info;
@@ -14,10 +18,5 @@ TRUNCATE TABLE bronze.erp_cust_az12;
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 TRUNCATE TABLE bronze.erp_sales_details;
 
-SELECT COUNT(*) FROM bronze.crm_cust_info;
-SELECT COUNT(*) FROM bronze.crm_prd_info;
-SELECT COUNT(*) FROM bronze.crm_sales_details;
-
-SELECT COUNT(*) FROM bronze.erp_cust_az12;
-SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2;
-SELECT COUNT(*) FROM bronze.erp_sales_details;
+END;
+$$;
